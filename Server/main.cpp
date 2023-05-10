70c639ef35307e45c754bd2cdcc7412f5373455c
Kokoulin Aleksei <kokoulin21@gmail.com>
2023-05-10 20:03:27 +0600

    Initial Commit

---
+++ Server/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Server
+//
+//  Created by Алексей Кокоулин on 10.05.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
